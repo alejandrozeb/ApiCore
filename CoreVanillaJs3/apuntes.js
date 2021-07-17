72f4042f7800434d800c84fe0ff6382c61b996f7
unknown <alejandrojzeballos@gmail.com>
2021-07-17 00:41:35 -0400

    {app} añadiendo los models

---
+++ CoreVanillaJs3/apuntes.js
@@ -0,0 +1,14 @@
+﻿/*
+ appsetting nos sirve para configurar
+ la app.
+ para usar entity framework tenemos que instalrlo en depdendencias
+ usamos administrador nugget y encontramos entity framework sql server
+ entity framework tools
+ para las migraciones 
+ 
+ para mapear una bd existente debemos hacerlo desde la consola nugget
+
+    Scaffold-DBContext "Server=Localhost;Database=CrudVanillaJs;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
+
+cadena para mappear la bd, tambien podemos usar User= Password= para usar sqlAutentication
+ */
